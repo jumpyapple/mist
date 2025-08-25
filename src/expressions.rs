f5ac57207b6bfe58dad731a8ef276eb637a4ad6c
jumpyapple <70656949+jumpyapple@users.noreply.github.com>
2025-08-25 18:48:52 -0400

    feat: rewrite tokens and partially rewrite expressions
    I just realize what token_type, expr_type and stmt_type are supposed to be. They are the tags for serde.

--- src/expressions.rs
+++ src/expressions.rs
@@ -1,5 +1,255 @@
-use crate::tokens::{Token, TokenType, ValueToken};
+use std::fmt;
+use crate::tokens::{NewToken, Token, TokenType, ValueToken};
 use serde::{Deserialize, Serialize};
+use crate::tokens::Token::TypeOnlyToken;
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[serde(tag = "expr_type")]
+pub enum NewExpression {
+    Literal { value: NewToken },
+    Named { name: NewToken },
+    Unary { operator: NewToken, right: Box<NewExpression> },
+    Binary { operator: NewToken, left: Box<NewExpression>, right: Box<NewExpression> },
+    Logical { operator: NewToken, left: Box<NewExpression>, right: Box<NewExpression> },
+    // Technically, only Named Identifier should be allowed as name.
+    Assign { name: Box<NewExpression>, value: Box<NewExpression> },
+    // Technically, only Named Identifier should be allowed as call.
+    Call { call: Box<NewExpression>, args: Vec<NewExpression> },
+    Grouping { expr: Box<NewExpression> },
+}
+
+impl NewExpression {
+    pub(crate) fn fmt_indented(
+        &self,
+        f: &mut std::fmt::Formatter<'_>,
+        indent: usize,
+    ) -> std::fmt::Result {
+        let current_indent = " ".repeat(indent * 4);
+        write!(f, "{}", current_indent)?;
+        match self {
+            NewExpression::Literal { value } => value.fmt_indented(f, indent),
+            NewExpression::Named { name } => name.fmt_indented(f, indent),
+            NewExpression::Unary { operator, right   } => {
+                operator.fmt_indented(f, 0)?;
+                right.fmt_indented(f, 0)
+            }
+            NewExpression::Binary { operator, left, right } => {
+                left.fmt_indented(f, 0)?;
+                operator.fmt_indented(f, 0)?;
+                right.fmt_indented(f, 0)
+            }
+            NewExpression::Logical { operator, left, right } => {
+                left.fmt_indented(f, 0)?;
+                operator.fmt_indented(f, 0)?;
+                right.fmt_indented(f, 0)
+            }
+            NewExpression::Assign { name, value } => {
+                name.fmt_indented(f, 0)?;
+                write!(f, " = ")?;
+                value.fmt_indented(f, 0)
+            }
+            NewExpression::Call { .. } => todo!(),
+            NewExpression::Grouping { .. } => todo!(),
+        }
+    }
+}
+
+impl fmt::Display for NewExpression {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.fmt_indented(f, 0)
+    }
+}
+
+#[test]
+fn test_expressions_de() {
+    // Literals: True, False, String, Number.
+    let input = r#"{"expr_type": "Literal", "value": {"token_type": "True"}}"#;
+    let result: NewExpression = serde_json::from_str(input).expect("failed to deserialize Expression::Literal (True)");
+    assert_eq!(result, NewExpression::Literal { value: NewToken::True } );
+
+    let input = r#"{"expr_type": "Literal", "value": {"token_type": "String", "value": "Hello Mistrian"}}"#;
+    let result: NewExpression = serde_json::from_str(input).expect("failed to deserialize Expression::Literal (String)");
+    assert_eq!(result, NewExpression::Literal { value: NewToken::String { value: "Hello Mistrian".to_owned() } } );
+
+    let input = r#"{"expr_type": "Literal", "value": {"token_type": "Number", "Value": 0.341}}"#;
+    let result: NewExpression = serde_json::from_str(input).expect("failed to deserialize Expression::Literal (String)");
+    assert_eq!(result, NewExpression::Literal { value: NewToken::Number { Value: 0.341 } } );
+
+    // Named.
+    let input = r#"{ "expr_type": "Named", "name": { "token_type": "Identifier", "value": "__get_new_day_spawn_x" }}"#;
+    let result: NewExpression = serde_json::from_str(input).expect("failed to deserialize Expression::Named");
+    let token = NewToken::Identifier { value: "__get_new_day_spawn_x".to_string(), default_value: None };
+    assert_eq!(result, NewExpression::Named { name: token } );
+
+    // Unary.
+    let input = r#"
+{
+    "expr_type": "Unary",
+    "operator": {"token_type": "Minus"},
+    "right": {
+        "expr_type": "Literal",
+        "value": {"token_type": "Number", "Value": 1.0}
+    }
+}
+    "#;
+    let result: NewExpression = serde_json::from_str(input).expect("failed to deserialize Expression::Unary");
+    let right_expr = NewExpression::Literal { value: NewToken::Number { Value: 1.0 } };
+    assert_eq!(result, NewExpression::Unary {operator: NewToken::Minus, right: Box::from(right_expr) });
+
+    // Binary.
+    let input = r#"
+{
+    "expr_type": "Binary",
+    "left": {
+        "expr_type": "Literal",
+        "value": {"token_type": "Number", "Value": 3.0}
+    },
+    "operator": {"token_type": "DoubleEqual"},
+    "right": {
+        "expr_type": "Literal",
+        "value": {"token_type": "Number", "Value": 0.0}
+    }
+}
+    "#;
+    let result: NewExpression = serde_json::from_str(input).expect("failed to deserialize Expression::Binary");
+    let left_expr = NewExpression::Literal { value: NewToken::Number { Value: 3.0 } };
+    let right_expr = NewExpression::Literal { value: NewToken::Number { Value: 0.0 } };
+    assert_eq!(result, NewExpression::Binary {operator: NewToken::DoubleEqual, left: Box::from(left_expr), right: Box::from(right_expr) });
+
+    // Logical.
+    let input  = r#"
+{
+    "expr_type": "Logical",
+    "left": {
+        "expr_type": "Binary",
+        "left": {
+            "expr_type": "Named",
+            "name": {
+                "token_type": "Identifier",
+                "value": "ari_health"
+            }
+        },
+        "operator": {"token_type": "DoubleEqual"},
+        "right": {
+            "expr_type": "Literal",
+            "value": {"token_type": "Number", "Value": 0.0}
+        }
+    },
+    "operator": {"token_type": "And"},
+    "right": {
+        "expr_type": "Binary",
+        "left": {
+            "expr_type": "Literal",
+            "value": {"token_type": "False"}
+        },
+        "operator": {"token_type": "DoubleEqual"},
+        "right": {
+            "expr_type": "Literal",
+            "value": {"token_type": "True"}
+        }
+    }
+}
+    "#;
+    let result: NewExpression = serde_json::from_str(input).expect("failed to deserialize Expression::Logical");
+    let left_expr = NewExpression::Binary {
+        operator: NewToken::DoubleEqual,
+        left: Box::from(NewExpression::Named { name: NewToken::Identifier { value: "ari_health".to_string(), default_value: None } }),
+        right: Box::from(NewExpression::Literal {value: NewToken::Number { Value: 0.0 } }),
+    };
+    let right_expr = NewExpression::Binary {
+        operator: NewToken::DoubleEqual,
+        left: Box::from(NewExpression::Literal {value: NewToken::False }),
+        right: Box::from(NewExpression::Literal {value: NewToken::True }),
+    };
+    assert_eq!(result, NewExpression::Logical {operator: NewToken::And, left: Box::from(left_expr), right: Box::from(right_expr) });
+
+    // Assign.
+    let input = r#"
+{
+    "expr_type": "Assign",
+    "name": {
+        "expr_type": "Named",
+        "name": {"token_type": "Identifier", "value": "lemonade"}
+    },
+    "value": {
+        "expr_type": "Literal",
+        "value": {"token_type": "True"}
+    }
+}
+    "#;
+    let result: NewExpression = serde_json::from_str(input).expect("failed to deserialize Expression::Assign");
+    assert_eq!(result, NewExpression::Assign {
+        name: Box::from(NewExpression::Named { name: NewToken::Identifier { value: "lemonade".to_string(), default_value: None } }),
+        value: Box::from(NewExpression::Literal { value: NewToken::True })
+    });
+
+    // Call.
+    let input = r#"
+{
+    "expr_type": "Call",
+    "call": {
+        "expr_type": "Named",
+        "name": {
+            "token_type": "Identifier",
+            "value": "get_response"
+        }
+    },
+    "args": []
+}
+    "#;
+    let result: NewExpression = serde_json::from_str(input).expect("failed to deserialize Expression::Call");
+    assert_eq!(result, NewExpression::Call {
+        call: Box::from(NewExpression::Named { name: NewToken::Identifier { value: "get_response".to_string(), default_value: None } }),
+        args: vec!(),
+    });
+
+    let input = r#"
+{
+    "expr_type": "Grouping",
+    "expr": {
+        "expr_type": "Binary",
+        "left": {
+            "expr_type": "Named",
+            "name": {
+                "token_type": "Identifier",
+                "value": "target_time"
+            }
+        },
+        "operator": {"token_type": "Minus"},
+        "right": {
+            "expr_type": "Named",
+            "name": {"token_type": "Identifier", "value": "start"}
+        }
+    }
+}
+    "#;
+    let result: NewExpression = serde_json::from_str(input).expect("failed to deserialize Expression::Grouping");
+    let left_expr = NewExpression::Named { name: NewToken::Identifier { value: "target_time".to_string(), default_value: None } };
+    let right_expr = NewExpression::Named { name: NewToken::Identifier { value: "start".to_string(), default_value: None} };
+    assert_eq!(result, NewExpression::Grouping {
+        expr: Box::from(NewExpression::Binary { operator: NewToken::Minus, left: Box::from(left_expr), right: Box::from(right_expr) }),
+    });
+}
+
+#[test]
+fn test_expression_format_human() {
+    // Literals.
+    let input= NewExpression::Literal { value: NewToken::True };
+    let ir = r#"{"expr_type": "Literal", "value": {"token_type": "True"}}"#;
+    assert_eq!(format!("{}", input), "true");
+
+    // Unary.
+    let input = NewExpression::Unary {operator: NewToken::Minus, right: Box::from(NewExpression::Literal { value: NewToken::Number { Value: 1.3 } }) };
+    assert_eq!(format!("{}", input), "-1.3");
+
+    // Assign.
+    let input = NewExpression::Assign {
+        name: Box::from(NewExpression::Named { name: NewToken::Identifier { value: "lemonade".to_string(), default_value: None } }),
+        value: Box::from(NewExpression::Literal { value: NewToken::True })
+    };
+    assert_eq!(format!("{}", input), "lemonade = true");
+
+}
 
 #[derive(Serialize, Deserialize)]
 pub enum ExpressionType {
@@ -26,6 +276,15 @@ pub enum Expression {
 }
 
 impl Expression {
+
+    pub(crate) fn new_boolean_literal(val: bool) -> Self {
+        Expression::Literal(LiteralExpression { expr_type: ExpressionType::Literal, value: Token::new_boolean(val) })
+    }
+
+    pub(crate) fn new_number_literal(val: f64) -> Self {
+        Expression::Literal(LiteralExpression { expr_type: ExpressionType::Literal, value: Token::new_number(val) })
+    }
+
     pub(crate) fn fmt_indented(
         &self,
         f: &mut std::fmt::Formatter<'_>,
@@ -91,7 +350,7 @@ impl NamedExpression {
 #[serde(deny_unknown_fields)]
 pub struct LiteralExpression {
     expr_type: ExpressionType, // Literal
-    value: Token,
+    pub value: Token,
 }
 
 impl LiteralExpression {
--- src/tokens.rs
+++ src/tokens.rs
@@ -1,5 +1,119 @@
-use crate::expressions::Expression;
+use std::fmt;
+use std::fmt::write;
+use crate::expressions::{Expression, NewExpression};
 use serde::{Deserialize, Serialize};
+use crate::MistContainer;
+use crate::tokens::Value::NumberValue;
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[serde(untagged)]
+pub enum NewDefaultValue {
+    #[serde(rename = "null")]
+    NullStringDefault, // The "null".
+    ExpressionDefault(Box<NewExpression>),
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[serde(tag = "token_type")]
+pub(crate) enum NewToken {
+    True,
+    False,
+    Null,
+    Number { Value: f64 },
+    String { value: String },
+    Identifier { value: String, default_value: Option<NewDefaultValue> },
+
+    // Operators.
+    DoubleEqual,
+    BangEqual,
+    LessEqual,
+    Less,
+    GreaterEqual,
+    Greater,
+    Plus,
+    Minus,
+    Star,
+    Slash,
+    And,
+
+    // Unary Operators.
+    Bang,
+}
+
+impl NewToken {
+    pub(crate) fn fmt_indented(
+        &self,
+        f: &mut std::fmt::Formatter<'_>,
+        indent: usize,
+    ) -> std::fmt::Result {
+        match self {
+            NewToken::True => write!(f, "true"),
+            NewToken::False => write!(f, "false"),
+            NewToken::Null => write!(f, "null"),
+            NewToken::Number { Value } => write!(f, "{}", Value),
+            NewToken::String { value } => write!(f, "\"{}\"", value),
+            NewToken::Identifier { value, default_value } => {
+                write!(f, "{}", value)?;
+                if let Some(v) = default_value {
+                    match v {
+                        NewDefaultValue::NullStringDefault => write!(f, "null")?,
+                        NewDefaultValue::ExpressionDefault(exp) => todo!(),
+                    };
+                }
+                Ok(())
+            }
+            NewToken::DoubleEqual => write!(f, "=="),
+            NewToken::BangEqual => write!(f, "!="),
+            NewToken::LessEqual => write!(f, "<="),
+            NewToken::Less => write!(f, "<"),
+            NewToken::GreaterEqual => write!(f, ">="),
+            NewToken::Greater => write!(f, ">"),
+            NewToken::Plus => write!(f, "+"),
+            NewToken::Minus => write!(f, "-"),
+            NewToken::Star => write!(f, "*"),
+            NewToken::Slash => write!(f, "/"),
+            NewToken::And => write!(f, "&&"),
+            NewToken::Bang => write!(f, "!"),
+        }
+    }
+}
+
+impl fmt::Display for NewToken {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.fmt_indented(f, 0)
+    }
+}
+
+#[test]
+fn test_tokens_de() {
+    let input = r#"{ "token_type": "Number", "Value": 0.99 }"#;
+    let result: NewToken = serde_json::from_str(input).expect("failed to deserialize Number");
+    assert_eq!(result, NewToken::Number { Value: 0.99 });
+
+    let input = r#"{ "token_type": "String", "value": "Music/Jingles/GoToSleep"}"#;
+    let result: NewToken = serde_json::from_str(input).expect("failed to deserialize String");
+    assert_eq!(result, NewToken::String { value: "Music/Jingles/GoToSleep".to_string() });
+
+    let input = r#"{"token_type": "Identifier", "value": "__get_new_day_spawn_x" }"#;
+    let result: NewToken = serde_json::from_str(input).expect("failed to deserialize Identifier");
+    assert_eq!(result, NewToken::Identifier { value: "__get_new_day_spawn_x".to_string(), default_value: None });
+
+    let input = r#"{ "token_type": "Identifier", "value": "dir", "default_value": "null" }"#;
+    let result: NewToken = serde_json::from_str(input).expect("failed to deserialize Identifier (with default value = \"null\")");
+    assert_eq!(result, NewToken::Identifier { value: "dir".to_string(), default_value: Some(NewDefaultValue::NullStringDefault) });
+
+    let input = r#"{"token_type": "DoubleEqual"}"#;
+    let result: NewToken = serde_json::from_str(input).expect("failed to deserialize DoubleEqual");
+    assert_eq!(result, NewToken::DoubleEqual);
+}
+
+#[test]
+fn test_token_format_human() {
+    let input = NewToken::String { value: "hello".to_string() };
+    assert_eq!(format!("{}", input), "\"hello\"");
+}
+
+
 
 #[derive(Serialize, Deserialize, Debug)]
 pub enum TokenType {
@@ -37,6 +151,22 @@ pub enum Token {
 }
 
 impl Token {
+    pub(crate) fn new_boolean(val: bool) -> Self {
+        if val {
+            Token::TypeOnlyToken(TypeOnlyToken { token_type: TokenType::BooleanTrue })
+        } else {
+            Token::TypeOnlyToken(TypeOnlyToken { token_type: TokenType::BooleanFalse })
+        }
+    }
+
+    pub(crate) fn new_number(val: f64) -> Self {
+        Token::ValueToken(ValueToken { token_type: TokenType::Number, value: Value::NumberValue(val) })
+    }
+
+    pub(crate) fn new_string(val: String) -> Self {
+        Token::ValueToken(ValueToken { token_type: TokenType::String, value: Value::StringValue(val) })
+    }
+
     pub(crate) fn fmt_indented(
         &self,
         f: &mut std::fmt::Formatter<'_>,
@@ -57,6 +187,7 @@ pub struct TypeOnlyToken {
 }
 
 impl TypeOnlyToken {
+
     pub(crate) fn fmt_indented(
         &self,
         f: &mut std::fmt::Formatter<'_>,
@@ -96,9 +227,9 @@ pub enum Value {
 #[derive(Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
 pub struct ValueToken {
-    token_type: TokenType, // Identifier (without default_value), Number, String.
+    pub token_type: TokenType, // Identifier (without default_value), Number, String.
     #[serde(alias = "Value")]
-    value: Value,
+    pub value: Value,
 }
 
 impl ValueToken {
